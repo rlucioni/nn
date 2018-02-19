9b6138c4c836309a2e8babf3e95257fd268e8e29
Renzo Lucioni <renzo@lucioni.xyz>
2018-02-19 17:28:49 -0500

    switch to tensorflow

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 .DEFAULT_GOAL := help
 .PHONY: requirements
 
-# Generates a help message. Borrowed from https://github.com/pydanny/cookiecutter-djangopackage.
-help: ## Display this help message
+help: ## display this help message
 	@echo "Run \`make <target>\` where <target> is one of"
 	@perl -nle'print $& if m{^[\.a-zA-Z_-]+:.*?## .*$$}' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m  %-25s\033[0m %s\n", $$1, $$2}'
 
-lint: ## Run flake8
+lint: ## run flake8
 	flake8 nn.py
 
-requirements: ## Install requirements
+requirements: ## install requirements
 	pip install -r requirements.txt
+
+run: ## run tf
+	TF_CPP_MIN_LOG_LEVEL=2 python nn.py
--- nn.py
+++ nn.py
@@ -1,187 +1,68 @@
-#!/usr/bin/env python
-import numpy as np
+"""
+simple MNIST classifier
+
+https://www.tensorflow.org/tutorials/layers
+https://www.tensorflow.org/versions/r0.12/tutorials/mnist/beginners/
+https://www.tensorflow.org/versions/r0.12/tutorials/mnist/pros/
+https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/mnist/mnist_softmax.py
+"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
 
-class NeuralNetwork:
-    """
-    Models a simple neural network.
-    """
-    def __init__(self, input_units, hidden_units, output_units, output_scaling_factor):
-        # network structure
-        self.input_units = input_units
-        self.hidden_units = hidden_units
-        self.output_units = output_units
+# mnist.train: 55,000 points of training data
+# mnist.test: 10,000 points of test data
+# mnist.validation: 5,000 points of validation data
+mnist = input_data.read_data_sets('mnist_data/', one_hot=True)
 
-        self.output_scaling_factor = output_scaling_factor
+# placeholder for flattened 28x28 = 784 pixel intensity vectors
+x = tf.placeholder(tf.float32, [None, 784])
 
-        # weight matrix from input layer to hidden layer
-        self.W1 = np.random.randn(self.input_units, self.hidden_units)
-        # weight matrix from hidden layer to output layer
-        self.W2 = np.random.randn(self.hidden_units, self.output_units)
+# variables to be learned
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 
-        self.hidden_sum = None
-        self.output_sum = None
+# model
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-    # TODO: Try rectified linear function
-    # http://ufldl.stanford.edu/tutorial/supervised/MultiLayerNeuralNetworks/
-    def sigmoid(self, x):
-        """Activation function"""
-        return 1 / (1 + np.exp(-x))
+# placeholder for correct labels
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-    def sigmoid_prime(self, x):
-        """Derivative of activation function, for gradient descent"""
-        return x * (1 - x)
+# loss to be minimized
+# cross-entropy explained at http://colah.github.io/posts/2015-09-Visual-Information/
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-    def forward(self, inputs):
-        """Forward propagation"""
-        self.hidden_sum = self.sigmoid(np.dot(inputs, self.W1))
-        self.output_sum = self.sigmoid(np.dot(self.hidden_sum, self.W2))
+# more optimizers at https://www.tensorflow.org/api_guides/python/train#optimizers
+learning_rate = 0.5
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
-    def backward(self, training_input, training_output):
-        """Backpropagation"""
-        output_error = training_output - self.output_sum
-        delta_output_sum = output_error * self.sigmoid_prime(self.output_sum)
+# define and run variable init operation
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
 
-        hidden_error = delta_output_sum.dot(self.W2.T)
-        delta_hidden_sum = hidden_error * self.sigmoid_prime(self.hidden_sum)
+steps = 1000
+for _ in range(steps):
+    # feed batches of 100 random training points to the train_step (stochastic training)
+    batch_xs, batch_ys = mnist.train.next_batch(100)
 
-        # adjust weights
-        self.W1 += training_input.T.dot(delta_hidden_sum)
-        self.W2 += self.hidden_sum.T.dot(delta_output_sum)
+    feed_dict = {
+        x: batch_xs,
+        y_: batch_ys,
+    }
+    sess.run(train_step, feed_dict=feed_dict)
 
-    def epoch(self, training_input, training_output):
-        self.forward(training_input)
-        self.backward(training_input, training_output)
+# get a list of booleans comparing predicted labels to correct labels
+predicted_labels = tf.argmax(y, 1)
+correct_labels = tf.argmax(y_, 1)
+correct_predictions = tf.equal(predicted_labels, correct_labels)
 
-    def mean_squared_error(self, target, output_sum):
-        return np.mean(np.square(target - output_sum))
+# determine fraction of correct predictions
+accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
-    def train(self, training_input, training_output, epochs):
-        print(f'normalized training input\n{training_input}')
-        print(f'normalized training output\n{training_output}')
-        print(f'scaled training output\n{training_output * self.output_scaling_factor}\n')
-
-        for epoch in range(epochs):
-            if epoch % 10 == 0:
-                print(f'epoch {epoch}')
-
-                self.epoch(training_input, training_output)
-                print(f'output sum\n{self.output_sum}')
-                print(f'scaled output sum\n{self.output_sum * self.output_scaling_factor}')
-
-                mse = self.mean_squared_error(training_output, self.output_sum)
-                print(f'mse: {mse}\n')
-            else:
-                self.epoch(training_input, training_output)
-
-    def test(self, test_input):
-        print(f'normalized test input: {test_input}')
-
-        self.forward(test_input)
-        print(f'output sum: {self.output_sum}')
-        print(f'scaled output sum: {self.output_sum * self.output_scaling_factor}')
-
-    # def dump_weights(self):
-    #     np.savetxt('w1.txt', self.W1, fmt='%s')
-    #     np.savetxt('w2.txt', self.W2, fmt='%s')
-
-
-def scores(epochs):
-    """
-    Predict exam scores given hours studied and hours slept.
-    """
-    # hours studied, hours slept
-    training_input = np.array(
-        [
-            [2, 9],
-            [1, 5],
-            [3, 6],
-        ],
-        dtype=float
-    )
-
-    # exam scores
-    training_output = np.array(
-        [
-            [92],
-            [86],
-            [89],
-        ],
-        dtype=float
-    )
-
-    # Normalize units by dividing by the maximum value for each variable.
-    # Inputs are in hours, outputs are exam scores out of 100.
-    normalized_input = training_input / np.amax(training_input, axis=0)
-    normalized_output = training_output / 100
-
-    nn = NeuralNetwork(2, 3, 1, 100)
-    nn.train(normalized_input, normalized_output, epochs)
-
-    test_input = np.array(
-        [
-            [4, 8],
-        ],
-        dtype=float
-    )
-    normalized_test_input = test_input / np.amax(test_input, axis=0)
-    nn.test(normalized_test_input)
-
-
-def digits(epochs):
-    """
-    Digit classification
-    """
-    zero = [
-        0, 1, 1, 0,
-        1, 0, 0, 1,
-        1, 0, 0, 1,
-        1, 0, 0, 1,
-        0, 1, 1, 0,
-    ]
-
-    one = [
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-    ]
-
-    two = [
-        0, 1, 1, 0,
-        1, 0, 0, 1,
-        0, 0, 1, 0,
-        0, 1, 0, 0,
-        1, 1, 1, 1,
-    ]
-
-    three = [
-        1, 1, 1, 1,
-        0, 0, 0, 1,
-        0, 1, 1, 1,
-        0, 0, 0, 1,
-        1, 1, 1, 1,
-    ]
-
-    training_input = np.array([zero, one, two, three], dtype=float)
-    # TODO: Use a one-hot encoding and cross entropy instead of mse
-    # to turn this into classification instead of regression.
-    training_output = np.array([[0], [1], [2], [3]], dtype=float)
-
-    # normalize units
-    normalized_output = training_output / 3
-
-    nn = NeuralNetwork(20, 20, 1, 3)
-    nn.train(training_input, normalized_output, epochs)
-
-    test = [
-        0, 1, 1, 0,
-        1, 0, 0, 1,
-        0, 0, 1, 0,
-        0, 1, 0, 0,
-        1, 1, 1, 1,
-    ]
-
-    test_input = np.array([test], dtype=float)
-    nn.test(test_input)
+feed_dict = {
+    x: mnist.test.images,
+    y_: mnist.test.labels,
+}
+test_accuracy = sess.run(accuracy, feed_dict=feed_dict)
+print(test_accuracy)
